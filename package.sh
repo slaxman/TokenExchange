37948cf4a7e910239ea33aad0a2a92ed3ed34d13
ScripterRon <Ronald.Hoffman6@gmail.com>
2016-09-03 19:03:14 -0400

    Add package script

---
+++ package.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#########################
+# Package TokenExchange #
+#########################
+
+if [ -z "$1" ] ; then
+  echo "You must specify the version to package"
+  exit 1
+fi
+
+VERSION="$1"
+
+cd package
+rm -R *
+mkdir -p addons/lib TokenExchange
+cp ../ChangeLog.txt ../LICENSE ../README.md ../blocknotify.sh ../token-exchange.properties TokenExchange
+cp ../target/TokenExchange-$VERSION.jar addons/lib
+zip -r TokenExchange-$VERSION.zip addons TokenExchange
+tar zcf TokenExchange-$VERSION.tar.gz addons TokenExchange
+exit 0
+
