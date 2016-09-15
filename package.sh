bd2b71ed1163a7a943593bcf6c4e84e4ae703dfc
ScripterRon <Ronald.Hoffman6@gmail.com>
2016-09-14 20:35:09 -0400

    Version 1.4.0

--- package.sh
+++ package.sh
@@ -11,12 +11,17 @@ fi
 
 VERSION="$1"
 
+if [ ! -d package ] ; then
+  mkdir package
+fi
+
 cd package
 rm -R *
 mkdir -p addons/lib TokenExchange
 cp ../ChangeLog.txt ../LICENSE ../README.md ../blocknotify.sh ../token-exchange.properties TokenExchange
 cp ../target/TokenExchange-$VERSION.jar addons/lib
 zip -r TokenExchange-$VERSION.zip addons TokenExchange
+dos2unix TokenExchange/ChangeLog.txt TokenExchange/LICENSE TokenExchange/README.md TokenExchange/blocknotify.sh TokenExchange/token-exchange.properties
 tar zcf TokenExchange-$VERSION.tar.gz addons TokenExchange
 exit 0
 
